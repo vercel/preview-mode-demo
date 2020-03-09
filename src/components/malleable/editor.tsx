5de12d571a2fc17b52a3cbf5c406821437c241e0
Joe Haddad <timer150@gmail.com>
2020-03-09 01:00:51 -0400

    Eliminate Editor from Initial Load

---
+++ src/components/malleable/editor.tsx
@@ -0,0 +1,36 @@
+import React, { useMemo, useState } from "react";
+import { createEditor, Node } from "slate";
+import { withHistory } from "slate-history";
+import { Editable, Slate, withReact } from "slate-react";
+import { ValidElementTypes } from "./index";
+
+export default function SlateWrapper({
+  As,
+  id,
+  className,
+  initialText,
+  noop
+}: {
+  As: ValidElementTypes;
+  id: string;
+  className: string;
+  initialText: string;
+  noop?: boolean;
+}) {
+  const originalState = useMemo(() => [{ children: [{ text: initialText }] }], [
+    initialText
+  ]);
+  const [value, setValue] = useState<Node[]>(originalState);
+
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
+
+  if (noop) return null;
+
+  return (
+    <As id={id} className={className}>
+      <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+        <Editable placeholder={"..."} />
+      </Slate>
+    </As>
+  );
+}
---
+++ src/components/malleable/index.tsx
@@ -0,0 +1,103 @@
+import React, { Suspense, useEffect, useMemo, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+
+export type FieldEdit = { id: string; innerText: string };
+
+export type ValidElementTypes =
+  | "p"
+  | "div"
+  | "span"
+  | "h1"
+  | "h2"
+  | "h3"
+  | "h4"
+  | "pre";
+
+const SlateWrapper = React.lazy(() => import("./editor"));
+
+function PreloadSlateWrapper() {
+  const el = useMemo(() => {
+    // This doesn't actually need to be in the DOM
+    const el = document.createElement("div");
+    el.setAttribute("hidden", "hidden");
+    return el;
+  }, []);
+
+  return createPortal(
+    <Suspense fallback={null}>
+      <SlateWrapper As="div" id="" className="" initialText="" noop />
+    </Suspense>,
+    el
+  );
+}
+
+export default function Malleable({
+  id,
+  isActive,
+  children,
+  as: As = "p",
+  className,
+  edits
+}: {
+  id: string;
+  isActive: boolean;
+  children: React.ReactNode;
+  as?: ValidElementTypes;
+  className?: string;
+  edits: FieldEdit[];
+}) {
+  const editedChildren = useMemo(
+    () => edits?.find(c => c?.id === id)?.innerText || children,
+    [edits, children, id]
+  );
+
+  const contentRef = useRef<HTMLElement>();
+  const [initialText, setInitialText] = useState<string>("");
+  useEffect(() => {
+    if (isActive || !contentRef.current) return;
+    setInitialText(contentRef.current.innerText);
+  }, [editedChildren, isActive, contentRef.current]);
+
+  if (As === "p") {
+    As = "div";
+    className += " p";
+  } else if (As === "span") {
+    className += " span";
+  }
+
+  if (isActive) {
+    return (
+      <Suspense
+        fallback={
+          <As
+            ref={contentRef as any}
+            className={className + " text-muted"}
+            id={id}
+          >
+            {editedChildren}
+          </As>
+        }
+      >
+        <SlateWrapper
+          As={As}
+          id={id}
+          className={className}
+          initialText={initialText}
+        />
+      </Suspense>
+    );
+  }
+  return (
+    <>
+      <As ref={contentRef as any} className={className} id={id}>
+        {editedChildren}
+      </As>
+      {// Detect application hydration
+      initialText !== "" && (
+        // Prerender the Slate editor to trigger `React.lazy` loading (thereby
+        // skipping fallback state)
+        <PreloadSlateWrapper />
+      )}
+    </>
+  );
+}
