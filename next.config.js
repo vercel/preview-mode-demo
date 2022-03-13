6d9ccdd0b36169f1ffd762ed07233f37ce66114a
Lee Robinson <lrobinson2011@gmail.com>
2022-03-13 12:02:18 -0500

    Upgrade

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+module.exports = withContentlayer()({
+  // swcMinify: true,
+  // reactStrictMode: true,
   async rewrites() {
     return [
       {
@@ -8,4 +13,10 @@ module.exports = {
       { source: "/r/:snapshotId", destination: "/api/share/:snapshotId" },
     ];
   },
-};
+  webpack: (config, { dev, isServer }) => {
+    console.log({ key: process.env.AWS_S3_ACCESS_KEY_ID });
+    console.log({ s: process.env.AWS_S3_SECRET_ACCESS_KEY });
+    console.log({ bucket: process.env.AWS_S3_BUCKET });
+    return config;
+  },
+});
